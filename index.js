790a84b8db7ea082ac987a39c712be4107a255f9
Nick Duran <nvduran@gmail.com>
2021-03-23 21:12:16 -0500

    initial question commit

---
+++ index.js
@@ -0,0 +1,132 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+
+const promptUser = () => {
+     return inquirer.prompt([
+          {
+               type: 'input',
+               name: 'title',
+               message: 'What is your project title?',
+               validate: titleInput => {
+                    if (titleInput) {
+                         return true;
+                    } else {
+                         console.log('Please enter the title');
+                         return false;
+                    }
+               }
+          },
+          {
+               type: 'input',
+               name: 'description',
+               message: 'Enter a project description',
+               validate: descriptionInput => {
+                    if (descriptionInput) {
+                         return true;
+                    } else {
+                         console.log('Please enter your description');
+                         return false;
+                    }
+               }
+          },
+          {
+               type: 'input',
+               name: 'installation',
+               message: 'Enter installation instructions',
+               validate: installationInput => {
+                    if (installationInput) {
+                         return true;
+                    } else {
+                         console.log('Please enter your installation info');
+                         return false;
+                    }
+               }
+          },
+          {
+               type: 'input',
+               name: 'usage',
+               message: 'Enter usage information',
+               validate: usageInput => {
+                    if (usageInput) {
+                         return true;
+                    } else {
+                         console.log('Please enter your usage info');
+                         return false;
+                    }
+               }
+          },
+          {
+               type: 'input',
+               name: 'contribution',
+               message: 'Enter contribution guidelines',
+               validate: contributionInput => {
+                    if (contributionInput) {
+                         return true;
+                    } else {
+                         console.log('Please enter your contribution info');
+                         return false;
+                    }
+               }
+          },
+          {
+               type: 'input',
+               name: 'test',
+               message: 'Enter test instructions',
+               validate: testInput => {
+                    if (testInput) {
+                         return true;
+                    } else {
+                         console.log('Please enter your test info');
+                         return false;
+                    }
+               }
+          },
+          {
+               type: 'checkbox',
+               name: 'license',
+               message: 'Would you like a license listed?',
+               choices: ['N/A', 'ISC', 'MIT']
+          },
+          {
+               type: 'input',
+               name: 'email',
+               message: 'Enter your email address',
+               validate: emailInput => {
+                    if (emailInput) {
+                         return true;
+                    } else {
+                         console.log('Please enter your email');
+                         return false;
+                    }
+               }
+          },
+          {
+               type: 'input',
+               name: 'github',
+               message: 'Enter your github username',
+               validate: githubInput => {
+                    if (githubInput) {
+                         return true;
+                    } else {
+                         console.log('Please enter your github info');
+                         return false;
+                    }
+               }
+          },
+     ]);
+};
+
+
+async function start() {
+     try {
+          const data = await promptUser();
+          console.log(data);
+
+     }
+     catch (err) {
+          console.log(err);
+     }
+};
+
+start();
